5687df714ecc8c5e8d296bdf57f25fa65bbab3f0
wangzhiguo123 <1027601417@qq.com>
2019-05-06 17:37:17 +0800

    2019.5.6 下午 （封装ajax）

---
+++ Project4/js/a.js
@@ -0,0 +1,15 @@
+let func = (name) => `Hello ${name}!`;
+func('Jack') // "Helllet a = 5;
+let b = 10;
+
+function tag(s, v1, v2) {
+    console.log(s[0]);
+    console.log(s[1]);
+    console.log(s[2]);
+    console.log(v1);
+    console.log(v2);
+
+    return "OK";
+}
+
+tag`Hello ${ a + b } world ${ a * b}`;o Jack!"
